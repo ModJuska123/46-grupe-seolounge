7d88bdf23d230029adbb7aab1bca529240a2820c
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-20 18:40:21 +0200

    Update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Services } from "./components/Services";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+
+
+function App() {
+  return (
+   <div>
+    <Header/>
+    <Services/>
+    <Footer/>
+   </div>
+  );
+}
+
+export default App;
