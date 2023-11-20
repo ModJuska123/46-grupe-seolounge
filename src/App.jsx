1450ac4cea56211e4662967081e591082afd532d
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-20 23:00:38 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import {DemoPage} from "./pages/Demo";
-import {HomePage} from "./pages/Home";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { DemoPage } from "./pages/Demo";
+import { HomePage } from "./pages/Home";
 
 function App() {
   return (
-   <>
-    <DemoPage />
-    <HomePage />
-   </>
+    <BrowserRouter>
+      <Routes>
+        <Route index path='/' element={<HomePage />} />
+        <Route path='/demo' element={<DemoPage />} />
+        {/* <Route path='/register' element={<RegisterPage />} /> */}
+        {/* <Route path='*' element={<NoPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export function Header() {
     return (
          <div className="container">
@@ -9,11 +11,9 @@ export function Header() {
             </div>
 
             <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="/" className="nav-link px-2">Home</a></li>
-                <li><a href="/" className="nav-link px-2">Features</a></li>
-                <li><a href="/" className="nav-link px-2">Pricing</a></li>
-                <li><a href="/" className="nav-link px-2">FAQs</a></li>
-                <li><a href="/" className="nav-link px-2">About</a></li>
+                <li><Link to="/" className="nav-link px-2">Home</Link></li>
+                <li><Link to="/demo" className="nav-link px-2">Demo</Link></li>
+                
             </ul>
 
             <div className="col-md-3 text-end">
