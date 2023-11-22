2aa7a801afe138b6f280ab5f13eb2e07580c8778
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-22 14:02:47 +0200

    update

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,18 @@
+import { Footer } from "../sections/Footer";
+import { Header } from "../sections/Header";
+import { Register } from "../sections/Register";
+import { Services } from "../sections/Services";
+import { Register } from "../sections/Register";
+
+export function ServicesPage() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Register>
+
+            </main>
+            <Footer />
+        </>
+    );
+}
