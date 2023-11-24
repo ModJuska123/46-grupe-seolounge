7f6bdcf2e5d210d3b393d7122fdbb3ea08795a8a
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-24 18:52:52 +0200

    Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,20 @@
+import { HowCanWeHelp } from "../sections/HowCanWeHelp";
 import { Footer } from "../sections/Footer";
 import { Header } from "../sections/Header";
 import { Services } from "../sections/Services";
+import { Hero } from "../sections/Hero";
+import { BusinessGrowth } from "../sections/BusinessGrowth";
+
 
 export function HomePage() {
     return (
         <>
             <Header />
             <main>
+                <Hero />
                 <Services sortBy="titleAZ" limit={1}/>
+                <BusinessGrowth/>
+                <HowCanWeHelp />
             </main>
             <Footer />
         </>
